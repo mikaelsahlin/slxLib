460a0383676c10eaf6ed9b0aa46e2757b8500ec3
Joakim Brannstrom <joakim.brannstrom@gmx.com>
2017-07-08 12:41:49 +0200

    Migrate to python3 print

--- functions/flushSystemInfo.py
+++ functions/flushSystemInfo.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 def flushSystemInfo(name,sysList):
-  print "running flushSystemInfo"
+  print("running flushSystemInfo")
   if issubclass(type(name),str):    
     for its in sysList:
       the_list = its.iterfind(name)
